92dd73968907a1d71fe78a976dc9538ec9f18d7f
GDKAYKY <108950475+GDKAYKY@users.noreply.github.com>
2025-04-19 16:52:01 -0300

    Update database-search-icon.svg, modify CSS for sidebar width, and adjust static web asset configurations

--- obj/Debug/net7.0/EchoSearch.AssemblyInfo.cs
+++ obj/Debug/net7.0/EchoSearch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EchoSearch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158c05b52b5dfc8f76ee738cc65e16c38e56a5b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e986f18f43bc1c8493b3f80151df150fb72def")]
 [assembly: System.Reflection.AssemblyProductAttribute("EchoSearch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EchoSearch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
